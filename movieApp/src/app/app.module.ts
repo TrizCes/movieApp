c550f1fa29391c7052c9dd6e079fbb9df08a078a
Beatriz Ceschini <biaceschini@gmail.com>
2023-03-07 17:11:56 -0300

    menu, movies e api

--- movieApp/src/app/app.module.ts
+++ movieApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Paginas/home/home.component';
+import { SerieComponent } from './components/serie/serie.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SerieComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ movieApp/src/environments/enviroments.ts
@@ -0,0 +1,5 @@
+export const enviroment = {
+  production: false,
+};
+
+export const API_PATH: string = "https://api.themoviedb.org/3/movie/popular?api_key=1ae52f5c280e23a023d81819b80e54c2&language=pt-BR&page=1";
