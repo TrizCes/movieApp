e7b1dd5613e8a3417ae2df84207ec86e11e24e20
Beatriz Ceschini <biaceschini@gmail.com>
2023-03-07 17:12:40 -0300

    movies

--- movieApp/src/app/app.module.ts
+++ movieApp/src/app/app.module.ts
@@ -4,15 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Paginas/home/home.component';
-import { SerieComponent } from './components/serie/serie.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SerieComponent,
-    MenuComponent
+    MenuComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
--- movieApp/src/app/components/movies/movies.component.spec.ts
+++ movieApp/src/app/components/movies/movies.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SerieComponent } from './serie.component';
+import { MoviesComponent } from './movies.component';
 
-describe('SerieComponent', () => {
-  let component: SerieComponent;
-  let fixture: ComponentFixture<SerieComponent>;
+describe('MoviesComponent', () => {
+  let component: MoviesComponent;
+  let fixture: ComponentFixture<MoviesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SerieComponent ]
+      declarations: [ MoviesComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SerieComponent);
+    fixture = TestBed.createComponent(MoviesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
